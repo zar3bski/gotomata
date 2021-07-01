0eee4319f95a9af5af0b08070411e4df5d22ebcd
zar3bski <zarebskidavid@gmail.com>
2021-07-01 14:10:39 +0200

    WIP: gol implementation

--- pkg/rule/rule.go
+++ pkg/rule/rule.go
@@ -1,20 +1,37 @@
 package rule
 
 import (
-	"fmt"
-
 	"github.com/zar3bski/gotomata/pkg/screen"
 )
 
+//https://play.golang.org/p/P-Dk0NH_vf
+
 func GameOfLife(last_state screen.ScreenState) screen.ScreenState {
 	new_state := last_state
-	for _, h := range new_state {
-		for _, cell := range h {
-			// implement logic here
-			// neighbours =
-			fmt.Print(cell, " ")
+	for h, line := range new_state {
+		for v, _ := range line {
+			count_alive := 0
+			neighbours := [8]bool{
+				last_state[h-1][v-1],
+				last_state[h-1][v],
+				last_state[h-1][v+1],
+				last_state[h][v-1],
+				last_state[h][v+1],
+				last_state[h+1][v-1],
+				last_state[h+11][v],
+				last_state[h+1][v+1],
+			}
+			for _, element := range neighbours {
+				if element {
+					count_alive += 1
+				}
+			}
+			if count_alive == 3 {
+				new_state[h][v] = true
+			} else if count_alive < 2 || count_alive > 3 {
+				new_state[h][v] = false
+			}
 		}
-		fmt.Println()
 	}
 	return new_state
 }
