cac9aad2a34078b9a4db1604ce2e849d0aff9caf
zar3bski <zarebskidavid@gmail.com>
2021-07-07 20:21:28 +0200

    WIP: screen dimensions

--- cmd/gotomata.go
+++ cmd/gotomata.go
@@ -11,11 +11,12 @@ import (
 func main() {
 	//TODO: work on display sizing https://stackoverflow.com/questions/16569433/get-terminal-size-in-go
 	dimensions := screen.GetTerminalSize()
-	screen := screen.NewScreen(200, 200, "random")
+	screen := screen.NewScreen(int(dimensions[0]), int(dimensions[1]), "random")
 	for i := 0; i < 2; i++ {
 		fmt.Print("\x0c", screen.Display())
 		rule.GameOfLife(screen)
 		time.Sleep(1 * time.Second)
 	}
-	fmt.Printf("out: %#v\n", string(dimensions))
+	// TODO: identify height and width
+	fmt.Printf("out: %d\n", dimensions[0])
 }
